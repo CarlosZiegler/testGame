7ba3f1233509274ba58974c8a2be4f8a7f3cae72
MRzsztk <rzeszutek.malgorzata@gmail.com>
2020-06-17 10:50:42 +0200

    first commit for deployment

--- collisionText.js
+++ collisionText.js
@@ -3,12 +3,22 @@ class CollisionText {
         this.x = game.player.x;
         this.y = game.player.y-10;
         this.text1 = `${food}  GI: ${index}`;
+        this.fade = 255;
+        this.fadeAmount = 1;
       }
       drawText() {
           if (this.y>-15) {
               this.y-=1;
               this.x-=0.5;
           }
+          if (this.fade<0) {
+              this.fadeAmount=1;
+          }
+          if (this.fade>255) {
+              this.fadeAmount=-10;
+          }
+          this.fade -= this.fadeAmount; 
+          fill(0, 0, 0, this.fade)
           text(this.text1, this.x, this.y);
       }
 }   
